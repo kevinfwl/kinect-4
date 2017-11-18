3965d778c021fc9475af51d3ae363deace7e2ffd
Ian Bantoto <lcbantoto@gmail.com>
2017-11-18 16:25:50 -0500

    Created client for testing, changed return state

--- backend/run.py
+++ backend/run.py
@@ -7,116 +7,95 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+# CONSTANTS
+RUN = 201
+TRN = 202
+INV = 203
+WIN = 204
+
 # STORAGE
 INFO = {
-    'state': {'round': 1, 'wait-confirm': 0, 'turn': 'PC'},
-    'game': None, 
+    'state': {'black':21, 'red':21, 'turn': 'red', 'ai-chance':0, 'game-over': False, 'wait-confirm': 0},
+    'game': c4.startGame('alpha beta pruning'), 
     'possible-moves': [],
     'human-moves': [],
     'ai-moves': []
 }
 
+# FUN(CTIONS)
+# -> Do a player move if it is valid
 def tryMove(human_move):
+    # retrieve possible moves
     INFO['possible-moves'] = c4.generate_moves(INFO['game'].state)
+
+    # make sure move is valid
     if not (0 <= human_move <= 6): return 'invalid'
     elif INFO['possible-moves'] == []: return 'tie'
     elif human_move not in INFO['possible-moves']: return 'invalid'
+
+    # move is valid, so apply it to state, and indicate that move was successful
     INFO['game'].state = c4.modify_state(INFO['game'].state, human_move, INFO['game'].human)
     return True
-
+# -> Finds the optimal move using Alpha Beta Pruning algo
 def findMove():
-    # Decide AI move
+    # Decide
+    INFO['game'] = c4.Connect4(board=INFO['game'].state)
     algorithm = c4.Alpha_Beta_Pruning.Minimax([INFO['game'].nodes, INFO['game'].edges], False)
     ai_move = int(algorithm.path[1][-1])
-
-    # Apply AI move
+    # Apply
     INFO['game'].state = c4.modify_state(INFO['game'].state, ai_move, INFO['game'].ai)
+# -> update state to reflect 
+def endTurn():
+    INFO['state'][INFO['state']['turn']] -= 1
+    INFO['state']['turn'] = 'red' if INFO['state']['turn'] == 'black' else 'black'
 
 # CLASSES
-
+# -> 
 class Board(Resource):
-    def get(self):
-        return INFO['game'].state.tolist(), 201
+    def get(self, move): # move number doesn't matter
+        return INFO['game'].state.tolist(), RUN # game board
 
-    def post(self, move):
-        if(move == 9):
-            INFO['state']['round'] += 1
-            findMove()
-            if INFO['game'].evaluate(INFO['game'].state, INFO['game'].ai, c4.generate_moves(INFO['game'].state) == 0) == c4.constant:
-                return INFO['game'].state.tolist(), 203 # ai win
-            return INFO['game'].state.tolist(), 201
-        else:
-            if(tryMove(move)):
-                INFO['state']['round'] += 1
-                if INFO['game'].evaluate(INFO['game'].state, INFO['game'].human, c4.generate_moves(INFO['game'].state) == 0) == -c4.constant:
-                    return INFO['game'].state.tolist(), 203 # human win
-                return INFO['game'].state.tolist(), 201
-            else:
-                return 'invalid', 201
-
+    def post(self, move): # make move
+        if not INFO['state']['game-over']:
+            if move == 9: # AI
+                if INFO['state']['turn'] == 'black':
+                    findMove()
+                    endTurn()
+                    if INFO['game'].evaluate(INFO['game'].state, INFO['game'].ai, c4.generate_moves(INFO['game'].state) == 0) == c4.constant:
+                        return INFO['game'].state.tolist(), WIN # ai won
+                    INFO['human-moves'] = INFO['game'].evaluate(INFO['game'].state, INFO['game'].human, INFO['possible-moves'] == [])
+                    INFO['ai-moves'] = INFO['game'].evaluate(INFO['game'].state, INFO['game'].ai, c4.generate_moves(INFO['game'].state) == 0)
+                    INFO['state']['ai-chance'] = INFO['ai-moves'] / (INFO['ai-moves'] + INFO['human-moves'])
+                    return INFO['game'].state.tolist(), RUN
+                return 'PC turn', TRN
+            else: # Player
+                if INFO['state']['turn'] == 'red':
+                    if(tryMove(move)):
+                        endTurn()
+                        if INFO['game'].evaluate(INFO['game'].state, INFO['game'].human, c4.generate_moves(INFO['game'].state) == 0) == -c4.constant:
+                            return INFO['game'].state.tolist(), WIN # human won
+                        return INFO['game'].state.tolist(), RUN
+                    else:
+                        return 'invalid', INV
+                return 'AI turn', TRN
+        return 'Game already over', WIN
+# -> 
 class Game(Resource):
-    def get(self):
+    def get(self): # grab game info
         return INFO['state']
     
-    def put(self):
+    def put(self): # initialize game
+        INFO['state'] = {'black':21, 'red':21, 'turn': 'red', 'ai-chance':0, 'game-over': False, 'wait-confirm': 0}
         INFO['game'] = c4.startGame('alpha beta pruning')
-        return '', 201
+        INFO['possible-moves'] = []
+        INFO['human-moves'] = []
+        INFO['ai-moves'] = []
+        return '', RUN
 
-# ADDS
-# api.add_resource(Resource, urls, endpoint=Resource.__name__.lower())
+# ADDS -> api.add_resource(Resource, urls, endpoint=Resource.__name__.lower())
 api.add_resource(Board, '/board/<int:move>')
 api.add_resource(Game, '/game')
 
 # RUN
 if __name__ == '__main__':
-    '''
-    def play(algo_selection):
-        while True:
-            human_move_values = []
-            ai_move_values = []
-            possible_human_moves = generate_moves(game.state)
-            try:
-                human_move = int(input('Please enter your next move (0-6): '))
-            except ValueError:
-                print('Invalid move. Please enter another move')
-                continue
-            if human_move == -1:
-                print('Thank you for playing!')
-                break
-            elif possible_human_moves == []:
-                print('TIE')
-                break
-            elif human_move not in possible_human_moves:
-                print('Invalid move. Please enter another move')
-                continue
-            game.state = modify_state(game.state, human_move, game.human)
-
-            print(notuglyprint(game.state))
-
-            if game.evaluate(game.state, game.human, generate_moves(game.state) == 0) == -constant:
-                print('HUMAN WON')
-                break
-
-            game = Connect4(board=game.state)
-            algorithm = Alpha_Beta_Pruning.Minimax([game.nodes, game.edges], False)
-            ai_move = int(algorithm.path[1][-1])
-
-            print('AI move:', ai_move)
-            game.state = modify_state(game.state, ai_move, game.ai)
-            print(notuglyprint(game.state))
-
-            if game.evaluate(game.state, game.ai, generate_moves(game.state) == 0) == constant:
-                print('AI WON')
-                break
-
-            human_move_value = game.evaluate(game.state, game.human, possible_human_moves == [])
-            human_move_values.append(human_move_value)
-            print('Human value:', human_move_value)
-
-            ai_move_value = game.evaluate(game.state, game.ai, generate_moves(game.state) == 0)
-            ai_move_values.append(ai_move_value)
-            print('AI value:', ai_move_value)
-            ai_win_prob = ai_move_value / (ai_move_value + human_move_value)
-            print('AI probability of winnning:', ai_win_prob)
-    '''
     app.run(debug=True)
